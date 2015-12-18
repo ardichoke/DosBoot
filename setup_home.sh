1bb11212e9d4a2bfd1ed8ae8b80df18fd50e3f8e
Ryan DeShone <ryan@deshone.net>
2015-12-18 15:38:44 -0500

    Add setup_home and update README

---
+++ setup_home.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+hcdir='~/homeconfig'
+date=$(date +%Y%m%d)
+if [ -d $hcdir ]; then
+    echo "Creating symlinks..."
+    for file in $(ls -A $hcdir); do
+        [[ "$file" == ".git" ]] && echo "Skipping $file" && continue
+        [ -e ~/$file ] && echo "Moving $file" && mv -fv ~/$file{,.${date}}
+        echo "Linking $file"
+        ln -sf ${hcdir}/${file} ~/${file}
+    done
+
+else
+    echo "$hcdir doesn't exist"
+    exit 1
+fi
