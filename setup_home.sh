44ccced7d63f25f86924b0da30816dfe4637809e
Ryan DeShone <ryan@deshone.net>
2019-05-31 10:12:43 -0400

    Minor script cleanup, add git submodule automation

--- setup_home.sh
+++ setup_home.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-hcdir='~/homeconfig'
+hcdir="~/homeconfig"
 date=$(date +%Y%m%d)
-if [ -d $hcdir ]; then
-    echo "Creating symlinks..."
-    for file in $(ls -A $hcdir); do
+if [ -d ${hcdir} ]; then
+    echo "Making symlinks..."
+    for file in $(ls -A "${hcdir}"); do
         [[ "$file" == ".git" ]] && echo "Skipping $file" && continue
-        [ -e ~/$file ] && echo "Moving $file" && mv -fv ~/$file{,.${date}}
+        [ -e "~/$file" ] && echo "Moving $file" && mv -fv ~/$file{,.${date}}
         echo "Linking $file"
-        ln -sf ${hcdir}/${file} ~/${file}
+        ln -sf "${hcdir}/${file}" "~/${file}"
     done
 
 else
-    echo "$hcdir doesn't exist"
+    echo "${hcdir} doesn't exist"
     exit 1
 fi
+git submodule init
+git submodule update
